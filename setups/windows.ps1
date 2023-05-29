f7ae6b0a9713f1e525e5b24757318c8afbb7fde9
thed24 <thed24@gmail.com>
2023-05-29 20:53:03 +1000

    Add PSReadLine change for bash completion

--- setups/windows.ps1
+++ setups/windows.ps1
@@ -4,6 +4,7 @@ winget install -e --id Git.Git --accept-package-agreements --accept-source-agree
 winget install -e --id Microsoft.VCRedist.2015+.x64 --accept-package-agreements --accept-source-agreements
 winget install -e --id Microsoft.DotNet.DesktopRuntime.7 --accept-package-agreements --accept-source-agreements
 winget install -e --id "Flow Launcher" --accept-package-agreements --accept-source-agreements
+winget install -e --id Starship.Starship --accept-package-agreements --accept-source-agreements
 
 # Install C++ compiler
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
@@ -14,3 +15,22 @@ Set-Location ~/AppData/Local
 New-Item -ItemType SymbolicLink -Path "~/AppData/Local/nvim" -Target "~/AppData/Local/configurations/nvim"
 New-Item -ItemType SymbolicLink -Path "~/AppData/Local/alacritty" -Target "~/AppData/Local/configurations/alacritty"
 
+# Setup profile
+$profilePath = $PROFILE.CurrentUserAllHosts
+New-Item -ItemType File -Path $profilePath -Force
+
+$linesToAdd = @"
+New-Alias v nvim
+
+Set-PSReadLineKeyHandler -Key Tab -Function Complete
+
+Invoke-Expression (&starship init powershell)
+"@
+
+Add-Content -Path $profilePath -Value $linesToAdd -Force
+
+# Setup starship
+Invoke-WebRequest -Uri "https://github.com/dracula/starship/archive/master.zip" -OutFile "$HOME\Downloads\starship.zip"
+Expand-Archive -Path "$HOME\Downloads\starship.zip" -DestinationPath "$HOME\.config" -Force
+Move-Item -Path "$HOME\.config\starship-master\starship.toml" -Destination "$HOME\.config\starship.toml" -Force
+Remove-Item -Path "$HOME\Downloads\starship.zip" -Force
